150ed95c7abc86ed619c946c5452508dd6f15949
Jorge Lopez <Jorge Lopez@JorgeL>
2024-10-07 21:37:59 -0600

    agregar tortilla

--- src/INICIO/Opcion5.java
+++ src/INICIO/Opcion5.java
@@ -34,6 +34,7 @@ public class Opcion5 extends javax.swing.JPanel {
     int marca28 = 0;
     int marca29 = 0;
     int marca30 = 0;
+    int marca31 = 0;
     int id_pedido = 0;
     int id_producto = 0;
     ArrayList<Integer> Num = new ArrayList<>();
@@ -62,6 +63,8 @@ public class Opcion5 extends javax.swing.JPanel {
         PorciMayonesa.setText(texto6);
         String texto7 = "<html><center><body>PORCION DE<br>QUESO MOSARELA</body></center></html>";
          Queso.setText(texto7);
+         String texto8 = "<html><center><body>TORTILLA<br>UNIDAD</body></center></html>";
+         torti.setText(texto8);
     }
 
     private void Limpiar() {
@@ -82,6 +85,7 @@ public class Opcion5 extends javax.swing.JPanel {
         Botton28.setBackground(Botrojo);
         Botton29.setBackground(Botrojo);
         Botton30.setBackground(Botrojo);
+        Botton31.setBackground(Botrojo);
         marca15 = 0;   
         marca16 = 0;    
         marca17 = 0;  
@@ -170,6 +174,8 @@ public class Opcion5 extends javax.swing.JPanel {
         PorciGuaca = new javax.swing.JLabel();
         Botton30 = new Clases.PanelRound();
         Quesadilla = new javax.swing.JLabel();
+        Botton31 = new Clases.PanelRound();
+        torti = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         cantidad = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
@@ -700,6 +706,40 @@ public class Opcion5 extends javax.swing.JPanel {
             .addComponent(Quesadilla, javax.swing.GroupLayout.DEFAULT_SIZE, 45, Short.MAX_VALUE)
         );
 
+        Botton31.setBackground(new java.awt.Color(255, 102, 102));
+        Botton31.setPreferredSize(new java.awt.Dimension(75, 45));
+        Botton31.setRoundBottomLeft(15);
+        Botton31.setRoundBottomRight(15);
+        Botton31.setRoundTopLeft(15);
+        Botton31.setRoundTopRight(15);
+        Botton31.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                Botton31MouseClicked(evt);
+            }
+        });
+
+        torti.setFont(new java.awt.Font("Arial", 1, 10)); // NOI18N
+        torti.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        torti.setText("TORTILLA UNIDAD");
+        torti.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tortiMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout Botton31Layout = new javax.swing.GroupLayout(Botton31);
+        Botton31.setLayout(Botton31Layout);
+        Botton31Layout.setHorizontalGroup(
+            Botton31Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Botton31Layout.createSequentialGroup()
+                .addComponent(torti, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 1, Short.MAX_VALUE))
+        );
+        Botton31Layout.setVerticalGroup(
+            Botton31Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(torti, javax.swing.GroupLayout.DEFAULT_SIZE, 45, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
@@ -730,7 +770,9 @@ public class Opcion5 extends javax.swing.JPanel {
                             .addComponent(Botton29, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(Botton30, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(Botton30, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(Botton31, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
@@ -762,7 +804,9 @@ public class Opcion5 extends javax.swing.JPanel {
                     .addComponent(Botton28, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(Botton29, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(Botton30, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(Botton30, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Botton31, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(23, Short.MAX_VALUE))
         );
 
@@ -1172,6 +1216,25 @@ public class Opcion5 extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_Botton30MouseClicked
 
+    private void tortiMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tortiMouseClicked
+        if (marca31 == 0) {
+            Botton31.setBackground(Botverde);
+            marca31 = 1;
+            Num.add(69);
+        } else {
+            Botton31.setBackground(Botrojo);
+            marca31 = 0;
+            int remov = Num.indexOf(69);
+            Num.remove(remov);
+        }
+        
+        
+    }//GEN-LAST:event_tortiMouseClicked
+
+    private void Botton31MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Botton31MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_Botton31MouseClicked
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private Clases.PanelRound BotonAgregar;
@@ -1191,6 +1254,7 @@ public class Opcion5 extends javax.swing.JPanel {
     private Clases.PanelRound Botton28;
     private Clases.PanelRound Botton29;
     private Clases.PanelRound Botton30;
+    private Clases.PanelRound Botton31;
     private javax.swing.JLabel ChoAhumado;
     private javax.swing.JLabel ChoArgenti;
     private javax.swing.JLabel ChoriBremen;
@@ -1212,5 +1276,6 @@ public class Opcion5 extends javax.swing.JPanel {
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JLabel torti;
     // End of variables declaration//GEN-END:variables
 }
