e59807841b0f4aa5329536468b23841d80dc0e70
it <it@SoporteIT.Anatekelectronics.local>
2024-11-13 06:08:02 -0600

    Agregar Pedidos

--- src/clas/BDConexion.java
+++ src/clas/BDConexion.java
@@ -7,6 +7,7 @@ package clas;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 /**
@@ -17,8 +18,8 @@ import javax.swing.JOptionPane;
 public class BDConexion {
    
    // public static final String URL = "jdbc:mysql://26.195.136.94:3306/elrey?useTimezone=true&serverTimezone=UTC";//ENCUENTRO
-   // public static final String URL = "jdbc:mysql://26.65.135.22:3306/elrey?useTimezone=true&serverTimezone=UTC";//ZONA4}26.65.135.22
-      public static final String URL = "jdbc:mysql://localhost:3306/elrey?useTimezone=true&serverTimezone=UTC";
+      public static final String URL = "jdbc:mysql://26.65.135.22:3306/elrey?useTimezone=true&serverTimezone=UTC";//ZONA4}26.65.135.22
+     //public static final String URL = "jdbc:mysql://localhost:3306/elrey?useTimezone=true&serverTimezone=UTC";
    // public static final String URL = "jdbc:mysql://26.175.57.65:3306/elrey?useTimezone=true&serverTimezone=UTC";//FARO
     //public static final String URL = "jdbc:mysql://26.178.84.253:3306/elrey?useTimezone=true&serverTimezone=UTC";//administrador
    public static final String USER = "elrey";
@@ -30,7 +31,7 @@ public class BDConexion {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = (Connection) DriverManager.getConnection(URL, USER, CLAVE);
-        }catch(Exception e){
+        }catch(ClassNotFoundException | SQLException e){
             System.out.println("Error: " + e.getMessage());
             JOptionPane.showMessageDialog(null,"ERROR: "+e);
         }
